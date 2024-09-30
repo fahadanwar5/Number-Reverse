682dc4c6bd96de50e84420e0eebc7f46c7fa1bf6
Fahad Anwar <officerops5@gmail.com>
2024-09-30 16:48:12 +0500

    Create NumberReverse.c

---
+++ NumberReverse.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int num1,num2,num3;
+printf("Enter the number 1 = \n");
+scanf("%d", &num1);
+printf("Enter the number 2 = \n");
+scanf("%d",&num2);
+printf("Enter the number 3 = \n");
+scanf("%d",&num3);
+
+printf("reverse = %d%d%d %d%d%d\n",num1,num2,num3,num3,num2,num1);
+return 0;
+}
